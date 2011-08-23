d3c8fa77e665b20e46a954bc36bcba7f626938b7
bogeyman.est <bogeyman.est@978091e5-9e63-aa89-c3a8-82e9e470d70a>
2011-08-23 07:49:50 +0000

    Updated to Deus-Ex update. Added recent changes dialog. Fixed Save item button.

---
+++ trunk/TF2Items/Changes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TF2Items
+{
+    public partial class Changes : Form
+    {
+        public Changes()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
